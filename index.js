2d7a2ab1be25104d1372623b6d3ac11f23def962
Susisu <susisu2413@yahoo.co.jp>
2017-04-22 20:26:06 +0900

    Lint

--- index.js
+++ index.js
@@ -5,74 +5,75 @@ const fs = require("fs");
 const Canvas = require("canvas");
 
 function trim(text) {
-    return text.replace(/^([ \u3000]*\n)+/, "")
-        .replace(/([ \u3000]*\n)+$/, "");
+  return text.replace(/^([ \u3000]*\n)+/, "")
+    .replace(/([ \u3000]*\n)+$/, "");
 }
 
 function render(text, style, options) {
-    const trimmed = trim(text);
+  const trimmed = trim(text);
 
-    const st     = style || {};
-    const margin = st.margin || 0;
-    const font            = st.font || "_serif";
-    const color           = st.color || "#000000";
-    const backgroundColor = st.backgroundColor || "#FFFFFF";
-    const marginTop       = typeof margin === "object" ? margin.top    || 0 : margin;
-    const marginBottom    = typeof margin === "object" ? margin.bottom || 0 : margin;
-    const marginLeft      = typeof margin === "object" ? margin.left   || 0 : margin;
-    const marginRight     = typeof margin === "object" ? margin.right  || 0 : margin;
+  const st = style || {};
+  const font    = st.font            || "_serif";
+  const color   = st.color           || "#000000";
+  const bgColor = st.backgroundColor || "#FFFFFF";
 
-    const op = options || {};
-    const compressionLevel = op.compressionLevel || 0;
+  const margin = st.margin || 0;
+  const marginTop    = typeof margin === "object" ? (margin.top    || 0) : margin;
+  const marginBottom = typeof margin === "object" ? (margin.bottom || 0) : margin;
+  const marginLeft   = typeof margin === "object" ? (margin.left   || 0) : margin;
+  const marginRight  = typeof margin === "object" ? (margin.right  || 0) : margin;
 
-    const canvas = new Canvas(0, 0);
-    const ctx    = canvas.getContext("2d");
+  const op = options || {};
+  const compressionLevel = op.compressionLevel || 0;
 
-    ctx.textBaseline = "top";
+  const canvas = new Canvas(0, 0);
+  const ctx    = canvas.getContext("2d");
 
-    ctx.font = font;
+  ctx.textBaseline = "top";
 
-    const tm      = ctx.measureText(trimmed);
-    const width   = tm.actualBoundingBoxRight   - tm.actualBoundingBoxLeft   + marginLeft + marginRight;
-    const height  = tm.actualBoundingBoxDescent - tm.actualBoundingBoxAscent + marginTop  + marginBottom;
-    canvas.width  = width;
-    canvas.height = height;
+  ctx.font = font;
 
-    ctx.fillStyle = backgroundColor;
-    ctx.fillRect(0, 0, width, height);
+  const tm = ctx.measureText(trimmed);
+  const w = tm.actualBoundingBoxRight   - tm.actualBoundingBoxLeft   + marginLeft + marginRight;
+  const h = tm.actualBoundingBoxDescent - tm.actualBoundingBoxAscent + marginTop  + marginBottom;
+  canvas.width  = w;
+  canvas.height = h;
 
-    ctx.fillStyle = color;
-    ctx.fillText(trimmed, marginLeft, marginTop);
+  ctx.fillStyle = bgColor;
+  ctx.fillRect(0, 0, w, h);
 
-    return new Promise((resolve, reject) => {
-        canvas.toBuffer(
-            (err, buf) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(buf);
-            },
-            compressionLevel,
-            canvas.PNG_FILTER_NONE
-        );
-    });
+  ctx.fillStyle = color;
+  ctx.fillText(trimmed, marginLeft, marginTop);
+
+  return new Promise((resolve, reject) => {
+    canvas.toBuffer(
+      (err, buf) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(buf);
+      },
+      compressionLevel,
+      canvas.PNG_FILTER_NONE
+    );
+  });
 }
 
 function renderToFile(filename, text, style, options) {
-    return render(text, style, options)
-        .then(buf =>
-            new Promise((resolve, reject) => {
-                fs.writeFile(filename, buf, err => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve();
-                });
-            })
-        );
+  return render(text, style, options)
+    .then(buf =>
+      new Promise((resolve, reject) => {
+        fs.writeFile(filename, buf, err => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      })
+    );
 }
 
 module.exports = {
-    render,
-    renderToFile
+  render,
+  renderToFile
 };
